7a57ca1117ef526d67103c4ef767d10d1c3c53c6
TOSHIBA <TOSHIBA@TOSHIPC>
2019-12-15 13:41:47 -0500

    Es un simple boton Actuialice el boton

--- src/presentacion/Presentacion.java
+++ src/presentacion/Presentacion.java
@@ -69,7 +69,7 @@ public class Presentacion extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        jLabel1.setText("hola");
+        jLabel1.setText("hola  como estás");
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
